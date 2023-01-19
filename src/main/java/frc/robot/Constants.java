4c18508a4039e0f3f5e735cfef6686fdb4e8877b
Robro1878 <roal1878@gmail.com>
2023-01-19 14:46:04 -0500

    Drive to Distance working

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -51,6 +51,10 @@ public final class Constants {
         public static final double GEAR_RATIO = 10.71; // Neo rotates 10.71 times for one rotation of the output
         public static final double WHEEL_DIAMETER_INCHES = 6.0; // Inches
         public static final double WHEEL_CIRCUMFERENCE_INCHES = WHEEL_DIAMETER_INCHES * Math.PI; // Abt 18.85 in.
+        public static final double TICKS_PER_WHEEL_REVOLUTION = ConSparkMax.POSITION_CONVERSION_FACTOR * GEAR_RATIO;
+        public static final double TICKS_PER_INCH = TICKS_PER_WHEEL_REVOLUTION / WHEEL_CIRCUMFERENCE_INCHES;
+        public static final double INCHES_PER_TICK = WHEEL_CIRCUMFERENCE_INCHES / TICKS_PER_WHEEL_REVOLUTION;
+
         public static final int kLeftMotor1Port = 3;
         public static final int kLeftMotor2Port = 5;
         public static final int kRightMotor1Port = 2;
