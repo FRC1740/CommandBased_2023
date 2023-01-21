ff98842c7066116b52c4c238141e9af1c6738f9b
Charles Estabrooks <charles@estabrooks.org>
2023-01-21 11:27:44 -0500

    Renamed Manipulator to Claw

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,8 +12,8 @@ import frc.robot.commands.AutoBalancePID;
 import frc.robot.commands.DriveToDistance;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
-import frc.robot.subsystems.Manipulator.LedMode;
-import frc.robot.subsystems.Manipulator;
+import frc.robot.subsystems.Claw.LedMode;
+import frc.robot.subsystems.Claw;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.TurnToAngleProfiled;
@@ -30,7 +30,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  protected final Manipulator m_manipulator = new Manipulator();
+  protected final Claw m_manipulator = new Claw();
   private final Command m_autoCommand = new DriveToDistance(36, m_robotDrive);
 
   // The driver's controller
