37276333e6196dcf8198cbbbd4a340dc59f8c6e6
Charles Estabrooks <charles@estabrooks.org>
2023-01-21 15:37:49 -0500

    Fleshing out the Arm subsystem and setpoint constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,7 +46,32 @@ public final class Constants {
     public static final class ConSparkMax {
         public static final double POSITION_CONVERSION_FACTOR = 42.0;
     }
-        
+    
+    public static final class ArmConstants{
+        public static final int kRotationLeaderMotorPort = 6;
+        public static final int kRotationFollowerMotorPort = 7;
+        public static final int kExtensionMotorPort = 8;
+
+        /*
+         * All Angles based on Horizontal = 0
+         * Starting Configuration: 112 deg (0); Fully Retracted
+         * Mid Node Scoring: 41 deg (71); 4"  Extension
+         * High Node Scoring: 37 deg (75); 28" Extension
+         * Low Node Scoring: -22 deg (134); 4" Extension
+         * Human player maybe same as mid-node (close)
+         */
+        public static final int kStowedAngle = 0;
+        public static final int kHighNodeAngle = 71; // WAG
+        public static final int kMidNodeAngle = 75; // WAG
+        public static final int kLowNodeAngle = 134; // WAG
+        public static final int kSubStationAngle = 75; // WAG
+
+        public static final int kStowedPosition = 0;
+        public static final int kHighNodePosition = 4; // WAG
+        public static final int kMidNodePosition = 28; // WAG
+        public static final int kLowNodePostion = 4; // WAG 
+        public static final int kSubStationPosition = 75; // WAG
+    }
     public static final class DriveConstants {
         public static final double GEAR_RATIO = 10.71; // Neo rotates 10.71 times for one rotation of the output
         public static final double WHEEL_DIAMETER_INCHES = 6.0; // Inches
