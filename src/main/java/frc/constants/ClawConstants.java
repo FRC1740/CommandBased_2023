331d9fef99ab24c304ee2671a8ad335e3264cbaf
FRC Team 1740 Deus Ex Coder <crephoto@users.noreply.github.com>
2023-01-28 12:39:38 -0500

    Converted Claw motor to TalonSRX; Imported ctri vendor lib (#66)

--- src/main/java/frc/constants/ClawConstants.java
+++ src/main/java/frc/constants/ClawConstants.java
@@ -8,4 +8,7 @@ public final class ClawConstants {
     public static final double EjectCubeSpeed = -InjectCubeSpeed;
     public static final double InjectConeSpeed = .3; // WAG
     public static final double ShutdownDelay = 1.0;
+    public static final int IntakePeakCurrentLimit = 35; // Amps
+    public static final int IntakePeakDurationLimit = 200; // mSec
+    public static final int IntakeContinuousCurrentLimit = 20; // Amps
 }
