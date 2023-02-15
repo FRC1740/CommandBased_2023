12036340803919ff36b609a0d49c095e7ee638ee
FRC Team 1740 Deus Ex Coder <crephoto@users.noreply.github.com>
2023-02-15 09:58:59 -0500

    Refactoring constants (#97)

---
+++ src/main/java/frc/constants/LEDConstants.java
@@ -0,0 +1,16 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.constants;
+
+/** Add your docs here. */
+public final class LEDConstants {
+    public static final int kCubeR = 50;
+    public static final int kCubeG = 0;
+    public static final int kCubeB = 100;
+    
+    public static final int kConeR = 100;
+    public static final int kConeG = 50;
+    public static final int kConeB = 0;
+}
