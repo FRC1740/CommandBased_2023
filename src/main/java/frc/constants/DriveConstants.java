43f0bee1fbc5c96f4b254fce11e412a5915a4d60
Robro1878 <roal1878@gmail.com>
2023-03-03 17:56:31 -0500

    SysID

--- src/main/java/frc/constants/DriveConstants.java
+++ src/main/java/frc/constants/DriveConstants.java
@@ -28,12 +28,12 @@ public class DriveConstants {
     public static final double kEncoderDistancePerPulse = DRIVE_POSITION_CONVERSION_FACTOR / (double)kEncoderCPR; 
 
     //sysid constants
-    public static final double ks = 0.1256; //Value for testbed
-    public static final double kv = 1.3475; //Value for testbed
-    public static final double ka = 0.18822; //Value for testbed
-    public static final double kPDriveVel = 0.076663; //Value for testbed
+    public static final double ks = 0.20375; //Value for testbed
+    public static final double kv = 2.9751; //Value for testbed
+    public static final double ka = 4.4502; //Value for testbed
+    public static final double kPDriveVel = 3.0748; //Value for testbed
 
-    public static final double kTrackWidthMeters = 0.6032; //22in. for testbot, 18 in. for actual robot
+    public static final double kTrackWidthMeters = Units.inchesToMeters(18); //22in. for testbot, 18 in. for actual robot
     public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackWidthMeters);
 
     public static final double kMaxSpeedMetersPerSecond = 1;
