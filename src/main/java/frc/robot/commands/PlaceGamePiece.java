eef5fc1bc9fc69f2a34628dfc36ccd4d68bffb53
Robro1878 <98357666+Robro1878@users.noreply.github.com>
2023-03-04 11:17:49 -0500

    Place Game Piece Command and auto stuff (#142)
    * Drive to apriltag command
    
    * SysID
    
    * PlaceGamePieceCommand and auto stuff

---
+++ src/main/java/frc/robot/commands/PlaceGamePiece.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.constants.ArmConstants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.ArmProfiledPIDSubsystem;
+import frc.robot.subsystems.ClawSubsystem;
+import frc.robot.subsystems.TelescopePIDSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class PlaceGamePiece extends SequentialCommandGroup {
+  /** Creates a new PlaceGamePiece. */
+  private TelescopePIDSubsystem m_telescope = new TelescopePIDSubsystem();
+  private ArmProfiledPIDSubsystem m_arm = new ArmProfiledPIDSubsystem();
+  private ClawSubsystem m_claw = new ClawSubsystem();
+
+  public PlaceGamePiece() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+      new InstantCommand(() -> m_telescope.setSetpoint(ArmConstants.kHighNodePosition)),
+      new InstantCommand(() -> m_arm.setGoal(ArmConstants.kHighNodeAngle)),
+      new WaitUntilCommand(m_arm::atGoal),
+      new WaitUntilCommand(m_telescope::atSetpoint),
+      new RunCommand(() -> m_claw.score()).withTimeout(1),
+      new InstantCommand(() -> m_telescope.setSetpoint(ArmConstants.kStowedPosition)),
+      new InstantCommand(() -> m_arm.setGoal(ArmConstants.kStowedAngle)),
+      new WaitUntilCommand(m_arm::atGoal),
+      new WaitUntilCommand(m_telescope::atSetpoint));
+    
+  }
+}
+
