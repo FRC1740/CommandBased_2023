561151ccaad750e67b16fe6e95e155248682e425
Rob Pellowski <23526472+RPellowski@users.noreply.github.com>
2023-03-05 18:04:49 -0500

    Start using RobotShare, a prerequisite for new Auto commands

--- src/main/java/frc/robot/commands/Auto_RB_2.java
+++ src/main/java/frc/robot/commands/Auto_RB_2.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import frc.robot.RobotShared;
 import frc.robot.commands.basic.ClawScore;
 import frc.robot.subsystems.DriveSubsystem;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import edu.wpi.first.math.util.Units;
