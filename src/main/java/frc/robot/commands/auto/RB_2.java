08404b81e4a1cafb24cd80f68e6cc4a4a31b041c
egqnjeff <egqnjeff@gmail.com>
2023-03-10 23:50:03 -0500

    Adjusted values based on pratice matches

--- src/main/java/frc/robot/commands/auto/RB_2.java
+++ src/main/java/frc/robot/commands/auto/RB_2.java
@@ -31,7 +31,7 @@ public class RB_2 extends SequentialCommandGroup {
       // Score the piece in the high position (Cube or Cone)
       // and stow the arm
       new AutoArmScoreHigh(), // Move Arm & Telescope to high node position
-      new WaitCommand(1),
+      new WaitCommand(1.5),
       new ParallelDeadlineGroup (
         new WaitCommand(0.5),
         new ClawScore()
@@ -43,7 +43,7 @@ public class RB_2 extends SequentialCommandGroup {
       ),
 
       // Drive to the charge station and balance
-      new DriveToDistance(Units.inchesToMeters(-30.0), m_drive),
+      new DriveToDistance(Units.inchesToMeters(-42.0), m_drive),
       // new DriveOnAndBalanceChargeStation(false, m_drive)
       new AutoBalancePID(m_drive),
 
